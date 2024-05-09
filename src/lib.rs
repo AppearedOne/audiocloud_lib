606056c207e886abe3744047250b43e74c3e3a5e
AppearedOne <cubase1@gmx.ch>
2024-05-09 15:40:34 +0200

    Added getting pack names

--- src/lib.rs
+++ src/lib.rs
@@ -52,9 +52,8 @@ pub struct SampleLibrary {
     pub name: String,
 }
 
-pub fn get_packs_metadata(lib: &SampleLibrary) -> Vec<PackInfo> {
-    //return lib.packs.into_iter().map(|p| p.meta).collect();
-    todo!();
+pub fn get_packs_metadata(lib: SampleLibrary) -> Vec<PackInfo> {
+    lib.packs.into_iter().map(|p| p.meta).collect()
 }
 
 pub fn use_sample_relevance(
